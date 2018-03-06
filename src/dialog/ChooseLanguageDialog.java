545cc9b9a8ba5ea425b312eb105739784e2d1f7d
JDNew <623893416@qq.com>
2018-03-06 17:15:18 +0800

    提交多选窗口

---
+++ src/dialog/ChooseLanguageDialog.java
@@ -0,0 +1,79 @@
+package dialog;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChooseLanguageDialog extends DialogWrapper{
+
+    private JCheckBox checkBox;
+    private JCheckBox checkBox1;
+    private JFrame jFrame;
+    public interface IConfirmListener{
+        void confirm();
+    }
+    private IConfirmListener iConfirmListener;
+
+    public void setiConfirmListener(IConfirmListener iConfirmListener) {
+        this.iConfirmListener = iConfirmListener;
+    }
+
+    public ChooseLanguageDialog(@Nullable Project project, String title , boolean canBeParent) {
+        super(project, canBeParent);
+        __init(title);
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        JPanel jGridPanel = new JPanel(new GridLayout(1, 2));
+        checkBox = new JCheckBox("English");
+        checkBox1 = new JCheckBox("中文");
+
+        jGridPanel.add(checkBox);
+        jGridPanel.add(checkBox1);
+
+        return jGridPanel;
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createSouthPanel() {
+        JPanel jSelectAllPanel = new JPanel();
+
+        JCheckBox cb_selectAll = new JCheckBox("选择全部");
+        JButton bt_confirm = new JButton("确定");
+        bt_confirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                iConfirmListener.confirm();
+            }
+        });
+        jSelectAllPanel.add(cb_selectAll);
+        jSelectAllPanel.add(bt_confirm);
+        return jSelectAllPanel;
+    }
+
+    @Override
+    protected void doOKAction() {
+        super.doOKAction();
+    }
+
+    @Override
+    public void doCancelAction() {
+        close(-1);
+    }
+
+    protected void __init(String title) {
+        setTitle(title);
+        init();
+    }
+
+}
