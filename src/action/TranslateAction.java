506093c3bd7c087f188e7c4b11b2ed2c1d4df358
JDNew <623893416@qq.com>
2018-03-08 10:48:34 +0800

    将请求功能移至后台任务执行

--- src/action/TranslateAction.java
+++ src/action/TranslateAction.java
@@ -1,46 +1,17 @@
 package action;
 
-
-import bean.AndroidString;
-import bean.BaiduTranslateResult;
-import com.google.gson.Gson;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
-
-import common.BaiduApp;
-import common.Constant;
 import dialog.ChooseLanguageDialog;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 import task.BackgroundTask;
-import util.HttpUtil;
-import util.MD5Util;
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class TranslateAction extends AnAction implements HttpUtil.ICallbackListener {
+public class TranslateAction extends AnAction {
 
     private VirtualFile chooseFile;
-    private HttpUtil httpUtil;
     private ChooseLanguageDialog chooseLanguageDialog;
     private Project project;
 
@@ -55,9 +26,8 @@ public class TranslateAction extends AnAction implements HttpUtil.ICallbackListe
         chooseFile = CommonDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
 //        System.out.println(chooseFile.getPath());
         project = CommonDataKeys.PROJECT.getData(e.getDataContext());
-        httpUtil = new HttpUtil();
-        httpUtil.setiCallbackListener(this);
-        chooseLanguageDialog = new ChooseLanguageDialog(project, "翻译", false);
+
+        chooseLanguageDialog = new ChooseLanguageDialog(project, "Translating strings.xml", false);
         chooseLanguageDialog.setiConfirmListener(new ChooseLanguageDialog.IConfirmListener() {
             @Override
             public void confirm() {
@@ -68,6 +38,15 @@ public class TranslateAction extends AnAction implements HttpUtil.ICallbackListe
 
         chooseLanguageDialog.show();
 
+    }
+
+    /**
+     * 操作strings.xml文件
+     *
+     * @param filePath
+     */
+    private void operateStringXmlFile(String filePath) {
+        new BackgroundTask(project, "write to xml", chooseFile ).queue();
     }
 
     /**
@@ -94,146 +73,8 @@ public class TranslateAction extends AnAction implements HttpUtil.ICallbackListe
         }
     }
 
-    private ArrayList<AndroidString> originStringList;
-    private int page;
-
-    /**
-     * 读取strings.xml文件
-     *
-     * @param filePath
-     */
-    private void operateStringXmlFile(String filePath) {
-
-        originStringList = getStringList(filePath);
-        List<AndroidString> subStringList;
-
-
-        int count = 1;
-        int limit = 5;
-        page = originStringList.size() / limit;
-        int fromIndex = 0;
-        int toIndex = limit;
-        StringBuilder translateString = new StringBuilder();
-        String url;
-
-//        for (int i = 0; i < stringList.size(); i++) {
-//            System.out.println(stringList.get(i).getValue());
-//        }
-//        for (int i = 0; i < page; i++) {
-//            subStringList = originStringList.subList(fromIndex, toIndex);
-//            System.out.println(subStringList.toString());
-//            for (int j = 0; j < subStringList.size(); j++) {
-//
-//                translateString = translateString.append(subStringList.get(j).getValue()).append("\\n");
-//            }
-
-//            System.out.println(translateString.toString().substring(0 , translateString.toString().length() - 2));
-
-//            url = appendUrl(translateString.toString().substring(0, translateString.toString().length() - 2));
-//            httpUtil.doGet(url);
-
-
-//            fromIndex = toIndex;
-//            toIndex = toIndex + limit;
-//            translateString.setLength(0);
-//        }
-
-        for (int i = 0; i < originStringList.size(); i++) {
-            url = appendUrl(originStringList.get(i).getValue());
-            httpUtil.doGet(url);
-        }
-
-        for (int i = 0; i < originStringList.size(); i++) {
-            translateResultList.get(i).setKey(originStringList.get(i).getKey());
-        }
-
-//        writeIntoXml(translateResultList);
-        new BackgroundTask(project, "write to xml", chooseFile , translateResultList).queue();
-    }
-
-    /**
-     * 遍历strings.xml里的所有内容，并存储到列表里
-     *
-     * @param chooseFile
-     * @return
-     */
-    private ArrayList<AndroidString> getStringList(String chooseFile) {
-        ArrayList<AndroidString> originStringList = new ArrayList<>();
-        AndroidString originAndroidString;
-
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        try {
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(new File(chooseFile));
-            NodeList nodeList = document.getElementsByTagName("string");
-//            System.out.println(nodeList.getLength());
-
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                String key = document.getElementsByTagName("string").item(i).getAttributes().getNamedItem("name")
-                        .getNodeValue();
-                String value = document.getElementsByTagName("string").item(i).getFirstChild().getNodeValue()
-                        .replace("\\n", " ");
-                //把内容里的\n符号去掉，因为会与上传多个文字的分隔符冲突
-                originAndroidString = new AndroidString(key, value);
-                originStringList.add(originAndroidString);
-            }
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return originStringList;
-    }
-
-    /**
-     * 拼接url地址
-     *
-     * @param translateString 要翻译的文本（如果是一次上传多个文本的话，中间要用 \n 分隔）
-     */
-    private String appendUrl(String translateString) {
-
-
-        String salt = String.valueOf(System.currentTimeMillis());
-
-        String rawData = BaiduApp.APP_ID + translateString + salt + BaiduApp.APP_SECRET;
-        String sign = MD5Util.md5Lower32(rawData);
-
-        String url = Constant.TRANSLATE_URL
-                + "?q=" + HttpUtil.encode(translateString)
-                + "&from=" + Constant.ZH
-                + "&to=" + Constant.EN
-                + "&appid=" + BaiduApp.APP_ID
-                + "&salt=" + salt
-                + "&sign=" + sign;
-
-        return url;
-    }
-
-    private List<AndroidString> translateResultList = new ArrayList<>();
-
-    @Override
-    public void success(String result) {
-        Gson gson = new Gson();
-        BaiduTranslateResult baiduTranslateResult = gson.fromJson(result, BaiduTranslateResult.class);
-        String dst = baiduTranslateResult.getTrans_result().get(0).getDst();
-//        System.out.println(dst);
-//        String values[] = dst.split("\\\\n");
-//        for (int i = 0; i < values.length; i++) {
-//            System.out.println(values[i].toString());
-//            translateResultList.add(new AndroidString(values[i] , values[i]));
-//        }
-
-        translateResultList.add(new AndroidString("", dst));
 
 
 
-    }
 
-    @Override
-    public void fail(String result) {
-
-    }
 }
--- src/task/BackgroundTask.java
+++ src/task/BackgroundTask.java
@@ -1,33 +1,124 @@
 package task;
 
 import bean.AndroidString;
+import bean.BaiduTranslateResult;
+import com.google.gson.Gson;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import common.BaiduApp;
 import common.Constant;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import util.HttpUtil;
+import util.MD5Util;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class BackgroundTask extends Task.Backgroundable {
+public class BackgroundTask extends Task.Backgroundable implements HttpUtil.ICallbackListener {
 
     private VirtualFile chooseFile;
-    private List<AndroidString> androidStringList;
-    public BackgroundTask(@Nullable Project project, @Nls @NotNull String title, VirtualFile virtualFile, List<AndroidString> translateResultList) {
+
+    private ArrayList<AndroidString> originStringList;
+    private HttpUtil httpUtil;
+    private List<AndroidString> translateResultList ;
+
+    public BackgroundTask(@Nullable Project project, @Nls @NotNull String title, VirtualFile virtualFile) {
         super(project, title);
         chooseFile = virtualFile;
-        androidStringList = translateResultList;
+        translateResultList = new ArrayList<>();
+        httpUtil = new HttpUtil();
+        httpUtil.setiCallbackListener(this);
+
     }
 
     @Override
     public void run(@NotNull ProgressIndicator progressIndicator) {
-         writeIntoXml(androidStringList);
+
+        originStringList = getStringList(chooseFile.getPath());
+        String url;
+        for (int i = 0; i < originStringList.size(); i++) {
+            url = appendUrl(originStringList.get(i).getValue());
+            httpUtil.doGet(url);
+        }
+
+        for (int i = 0; i < originStringList.size(); i++) {
+            translateResultList.get(i).setKey(originStringList.get(i).getKey());
+        }
+         writeIntoXml(translateResultList);
+    }
+
+    /**
+     * 遍历strings.xml里的所有内容，并存储到列表里
+     *
+     * @param chooseFile
+     * @return
+     */
+    private ArrayList<AndroidString> getStringList(String chooseFile) {
+        ArrayList<AndroidString> originStringList = new ArrayList<>();
+        AndroidString originAndroidString;
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(new File(chooseFile));
+            NodeList nodeList = document.getElementsByTagName("string");
+//            System.out.println(nodeList.getLength());
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                String key = document.getElementsByTagName("string").item(i).getAttributes().getNamedItem("name")
+                        .getNodeValue();
+                String value = document.getElementsByTagName("string").item(i).getFirstChild().getNodeValue()
+                        .replace("\\n", " ");
+                //把内容里的\n符号去掉，因为会与上传多个文字的分隔符冲突
+                originAndroidString = new AndroidString(key, value);
+                originStringList.add(originAndroidString);
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return originStringList;
+    }
+
+    /**
+     * 拼接url地址
+     *
+     * @param translateString 要翻译的文本（如果是一次上传多个文本的话，中间要用 \n 分隔）
+     */
+    private String appendUrl(String translateString) {
+
+
+        String salt = String.valueOf(System.currentTimeMillis());
+
+        String rawData = BaiduApp.APP_ID + translateString + salt + BaiduApp.APP_SECRET;
+        String sign = MD5Util.md5Lower32(rawData);
+
+        String url = Constant.TRANSLATE_URL
+                + "?q=" + HttpUtil.encode(translateString)
+                + "&from=" + Constant.ZH
+                + "&to=" + Constant.EN
+                + "&appid=" + BaiduApp.APP_ID
+                + "&salt=" + salt
+                + "&sign=" + sign;
+
+        return url;
     }
 
     /**
@@ -66,6 +157,20 @@ public class BackgroundTask extends Task.Backgroundable {
             e.printStackTrace();
         }
 
+
+    }
+
+    @Override
+    public void success(String result) {
+        Gson gson = new Gson();
+        BaiduTranslateResult baiduTranslateResult = gson.fromJson(result, BaiduTranslateResult.class);
+        String dst = baiduTranslateResult.getTrans_result().get(0).getDst();
+
+        translateResultList.add(new AndroidString("", dst));
+    }
+
+    @Override
+    public void fail(String result) {
 
     }
 }
